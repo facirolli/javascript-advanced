6eb9633462d16ed1e0d96106992ec7df2bdf5e5b
Ricardo <rssfacirolli@hotmail.com>
2024-05-02 13:13:30 -0300

    fibonaccihtml

--- aula16/funcao2.js
+++ aula16/funcao2.js
@@ -1,8 +1,29 @@
+function fibonacci(){
+    let termo = Number(document.getElementById('numero').value)
+    let resposta = document.getElementById('resposta')
+    let penultimo = 0
+    let ultimo = 1
+    let numero
+    if(termo <= 2){
+        numero = termo -1
+    } else{
+        for(let i = 2; i < termo; i++){
+            numero = ultimo + penultimo
+            penultimo = ultimo
+            ultimo = numero
+        }
+    }
+    resposta.innerHTML = numero
+    }
+
+/*
 function fibonacci(n){
-    const fib = [0, 1]
+    let fib = [0, 1]
     for(let i = 2; i < n; i++){
     fib[i] = fib[i-1] + fib[i-2]
     }
     return fib
-}
-console.log(fibonacci(7))
+  
+}    
+console.log(fibonacci(10))
+*/
